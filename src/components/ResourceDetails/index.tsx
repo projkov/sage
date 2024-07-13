80b30e6d2892f568b8bb9ae472d0a92dcafb05d0
nvim <prozskov@gmail.com>
2024-07-13 20:29:51 +0200

    Add an ability to check JSON data

--- src/components/ResourceDetails/index.tsx
+++ src/components/ResourceDetails/index.tsx
@@ -1,13 +1,14 @@
 import React, { useState } from 'react';
-import { Button, Drawer } from 'antd';
+import { Button, Drawer, Tabs } from 'antd';
 import { FhirResource } from 'fhir/r4';
+import { Editor } from '@monaco-editor/react';
 
 interface ResourceDetailsProps {
     buttonText: string;
     resource: FhirResource;
 }
 
-export function ResourceDetails(props: ResourceDetailsProps){
+export function ResourceDetails(props: ResourceDetailsProps) {
     const [open, setOpen] = useState(false);
     const { buttonText, resource } = props;
 
@@ -26,8 +27,10 @@ export function ResourceDetails(props: ResourceDetailsProps){
             <Button type="link" onClick={showDrawer}>
                 {buttonText}
             </Button>
-            <Drawer title={drawerTitle} onClose={onClose} open={open} width={720}>
-                {JSON.stringify(resource, null, 2)}
+            <Drawer title={drawerTitle} onClose={onClose} open={open} width={900}>
+                <Tabs defaultActiveKey="1" items={[
+                    {key: '1', label: 'JSON', 'children': <><Editor height="90vh" defaultLanguage="json" defaultValue={JSON.stringify(resource, null, 2)} /></>}
+                ]} />
             </Drawer>
         </>
     );
