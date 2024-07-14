d0cf8c5acb748b9738733ada916ad2d7570629ef
nvim <prozskov@gmail.com>
2024-07-14 12:34:10 +0200

    Add render error

--- src/components/ResourceContent/index.tsx
+++ src/components/ResourceContent/index.tsx
@@ -3,13 +3,31 @@ import { FhirResource, Bundle } from "fhir/r4";
 import { useResourceInfo } from "../../hooks";
 import { ResourceTable } from "../ResourceTable";
 import { ResourceContentProps } from "./interfaces";
+import { Result } from "antd";
 
 export function ResourceContent(props: ResourceContentProps) {
-    const { resourceType, resourceConfig, serverUrl} = props;
+    const { resourceType, resourceConfig, serverUrl } = props;
     const { response } = useResourceInfo(resourceType, serverUrl)
 
     return (
-        <RenderRemoteData remoteData={response}>
+        <RenderRemoteData remoteData={response} renderFailure={() => {
+            if (serverUrl === '') {
+                return <Result
+                    style={{ marginTop: '150px' }}
+                    title="Please enter the FHIR server URL to establish a connection."
+                    extra={[
+                        'Make sure the URL is correct and accessible to ensure successful integration.'
+                    ]}
+                />
+            }
+
+            return <Result
+                style={{ marginTop: '150px' }}
+                status="error"
+                title="Something went wrong"
+                extra={["The FHIR server URL you provided is incorrect or there was an issue connecting to the server. Please check the URL and try again."]}
+            />
+        }}>
             {(bundle) => (
                 <ResourceTable
                     bundle={bundle as Bundle<FhirResource>}
