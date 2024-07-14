c8d670b58e39caf38fd42d1f72cbcb390c862e6e
nvim <prozskov@gmail.com>
2024-07-14 12:37:40 +0200

    Add is loading state

--- src/components/ResourceContent/index.tsx
+++ src/components/ResourceContent/index.tsx
@@ -3,31 +3,35 @@ import { FhirResource, Bundle } from "fhir/r4";
 import { useResourceInfo } from "../../hooks";
 import { ResourceTable } from "../ResourceTable";
 import { ResourceContentProps } from "./interfaces";
-import { Result } from "antd";
+import { Result, Spin } from "antd";
+import { LoadingOutlined } from "@ant-design/icons";
 
 export function ResourceContent(props: ResourceContentProps) {
     const { resourceType, resourceConfig, serverUrl } = props;
     const { response } = useResourceInfo(resourceType, serverUrl)
 
     return (
-        <RenderRemoteData remoteData={response} renderFailure={() => {
-            if (serverUrl === '') {
+        <RenderRemoteData
+            remoteData={response}
+            renderFailure={() => {
+                if (serverUrl === '') {
+                    return <Result
+                        style={{ marginTop: '150px' }}
+                        title="Please enter the FHIR server URL to establish a connection."
+                        extra={[
+                            'Make sure the URL is correct and accessible to ensure successful integration.'
+                        ]}
+                    />
+                }
+
                 return <Result
                     style={{ marginTop: '150px' }}
-                    title="Please enter the FHIR server URL to establish a connection."
-                    extra={[
-                        'Make sure the URL is correct and accessible to ensure successful integration.'
-                    ]}
+                    status="error"
+                    title="Something went wrong"
+                    extra={["The FHIR server URL you provided is incorrect or there was an issue connecting to the server. Please check the URL and try again."]}
                 />
-            }
-
-            return <Result
-                style={{ marginTop: '150px' }}
-                status="error"
-                title="Something went wrong"
-                extra={["The FHIR server URL you provided is incorrect or there was an issue connecting to the server. Please check the URL and try again."]}
-            />
-        }}>
+            }}
+            renderLoading={() => <Spin indicator={<LoadingOutlined spin />} size="large" style={{ marginTop: '250px' }} />}>
             {(bundle) => (
                 <ResourceTable
                     bundle={bundle as Bundle<FhirResource>}
